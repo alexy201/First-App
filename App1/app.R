9eb6b950a0aaca6981a4246bdf0c31f91e888bcc
alexy201 <alexy200412@gmail.com>
2020-07-26 23:51:24 -0700

    e

--- App1/app.R
+++ App1/app.R
@@ -56,7 +56,7 @@ server <- function(input, output) {
         x    <- faithful$waiting
         bins <- seq(min(x), max(x), length.out = input$bins + 1)
         
-        hist(x, breaks = bins, col = "#75AADB", border = "orange",
+        hist(x, breaks = bins, col = "#75AADB", border = "red",
              xlab = "Waiting time to next eruption (in mins)",
              main = "Histogram of waiting times")
         
