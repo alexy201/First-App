f1a272e4b7c761620d774c61653c988bb830fc73
alexy201 <alexy200412@gmail.com>
2020-07-27 13:34:14 -0700

    Starting work on server.R for new census app. Going on to lesson 5 next

---
+++ census-app/server.R
@@ -0,0 +1,23 @@
+#
+# This is the server logic of a Shiny web application. You can run the
+# application by clicking 'Run App' above.
+#
+# Find out more about building applications with Shiny here:
+#
+#    http://shiny.rstudio.com/
+#
+
+library(shiny)
+
+# Define server logic required to draw a histogram
+shinyServer(function(input, output) {
+
+    output$selected_var <- renderText({ 
+        paste("You have selected ", input$var)
+    })
+    
+    output$selected_range <- renderText({ 
+        paste("You have chosen a range from ", input$range[1], " to ", input$range[2])
+    })
+
+})
